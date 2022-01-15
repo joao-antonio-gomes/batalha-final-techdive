a0a910354f0fa57e25b421775a9347fc4e88ed02
João Gomes <joaoantoniog96@gmail.com>
2022-01-15 19:29:57 -0300

    Adicionado metodo atacar classe Armadilha

--- src/model/Armadilha.java
+++ src/model/Armadilha.java
@@ -1,6 +1,7 @@
 package model;
 
 import enums.ArmasEnum;
+import utils.Dado;
 
 public class Armadilha implements Atacante {
     private int pontosDeAtaque;
@@ -8,11 +9,28 @@ public class Armadilha implements Atacante {
 
     public Armadilha() {
         this.arma = ArmasEnum.ARMADILHA;
-        this.pontosDeAtaque = this.arma.getDano();
+        this.pontosDeAtaque = 5;
     }
 
     @Override
-    public void atacar(Personagem p) {
+    public void atacar(Personagem personagem) {
+        int ataqueByDado = Dado.rolarDado(10);
+        if (ataqueByDado == 1) {
+            System.out.println("O ataque da armadilha pegou de raspão e você não sofreu dano");
+            return;
+        }
 
+        int ataqueArma = arma.getDano();
+        int ataqueTotal = ataqueByDado + ataqueArma + pontosDeAtaque;
+
+        int pontosDeDefesaAlvo = personagem.getPontosDeDefesa();
+        int dano = ataqueTotal - pontosDeDefesaAlvo;
+        dano = Math.max(dano, 0);
+
+        int pontosDeVidaAlvo = personagem.getPontosDeVida();
+        int vidaRestante = pontosDeVidaAlvo - dano;
+        personagem.setPontosDeVida(vidaRestante);
+
+        System.out.println("Você sofreu " + dano + " de dano e agora possui " + vidaRestante + " pontos de vida");
     }
 }
